67a95005e97a9f2d528f191fe25090efa3234a9a
Thamsanqa N <Thamsanqa140102@gmail.com>
2021-06-15 02:29:50 +0200

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIF6teWsKH2QJKdAbMCu6pJUMLkJwjfEA",
-  authDomain: "facebook-clone-cd13c.firebaseapp.com",
-  projectId: "facebook-clone-cd13c",
-  storageBucket: "facebook-clone-cd13c.appspot.com",
-  messagingSenderId: "138256371076",
-  appId: "1:138256371076:web:7c0462e011d325212221d1",
-  measurementId: "G-HG3PJWK38M",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
